1c7987216c1e982686a38a717631d940c7cbd142
Dan Choiniere <dchoinie@gmail.com>
2020-02-19 19:41:30 -0600

    2nd phone added to hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,7 +3,6 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { FaAngleRight } from "react-icons/fa"
 import { useSpring, animated } from "react-spring"
-import VideoGroup from "../components/videoGroup"
 
 const Hero = () => {
   const fade = useSpring({
@@ -28,11 +27,19 @@ const Hero = () => {
           }
         }
       }
+      phone2: file(relativePath: { eq: "phones/iPhone4.png" }) {
+        childImageSharp {
+          fluid {
+            src
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
   return (
     <div
-      className="flex justify-center h-screen"
+      className="flex justify-center h-screen pb-12"
       style={{
         height: "calc(100vh - 85.55px)",
       }}
@@ -65,6 +72,7 @@ const Hero = () => {
               style={{
                 padding: "0.25rem 0.5rem",
                 backgroundColor: "#01bdc8",
+                border: "1px solid transparent",
               }}
             />
           </form>
@@ -79,64 +87,18 @@ const Hero = () => {
           fluid={data.phone.childImageSharp.fluid}
           style={{ maxWidth: "275px" }}
         />
+        <Img
+          className="w-full self-center"
+          fluid={data.phone2.childImageSharp.fluid}
+          style={{
+            maxWidth: "275px",
+            marginLeft: "-115px",
+            marginTop: "100px",
+          }}
+        />
       </div>
     </div>
   )
 }
 
 export default Hero
-
-{
-  /* <div className="flex flex-col justify-center self-center w-full px-12 lg:px-0 lg:w-1/2 ">
-        <div className="self-center">
-          <h1
-            className="text-4xl md:text-5xl lg:text-6xl text-gray-200 lowercase tracking-wide whitespace-no-wrap text-center md:text-left"
-            style={{ lineHeight: "1" }}
-          >
-            {data.site.siteMetadata.title}
-          </h1>
-          <animated.h3
-            className="text-xl lg:text-2xl text-gray-700 text-center md:text-left"
-            style={fade}
-          >
-            {data.site.siteMetadata.description}
-          </animated.h3>
-          <p className="text-gray-300 text-center md:text-left">
-            Get on the list now to be the first to test our Beta App.
-          </p>
-          <form
-            action="https://formspree.io/xgekgznv"
-            method="POST"
-            className="py-2 flex"
-          >
-            <input
-              type="email"
-              name="email"
-              className="text-xl rounded-l"
-              placeholder="Email"
-              style={{
-                padding: "0.25rem 0.5rem",
-              }}
-            />
-            <input
-              type="submit"
-              value="Get Savi"
-              className="text-xl fredoka bg-gray-700 text-white cursor-pointer rounded-r"
-              style={{
-                padding: "0.25rem 0.5rem",
-              }}
-            />
-          </form>
-          <p className="text-gray-400 self-center py-1 text-center md:text-left">
-            <em>Launching Spring 2020</em>
-          </p>
-        </div>
-      </div>
-      <div className="hidden md:flex md:w-1/2 md:justify-center self-center">
-        <Img
-          className="self-center w-full"
-          fluid={data.phone.childImageSharp.fluid}
-          style={{ maxWidth: "275px" }}
-        />
-      </div> */
-}
