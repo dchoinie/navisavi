71cd1b6f28d3841c01984b2e9d8bfaed719359ab
Luca Koval <lucakoval@me.com>
2020-07-30 12:27:57 -0400

    Fixing minor issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,17 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
-import Learn from "../components/learn"
 import Features from "../components/features"
 import Earn from "../components/earn"
-import Download from "../components/download"
 import InstaFeed from "../components/instaFeed"
 import Contact from "../components/contact"
-import Partnership from "../components/partnership"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <Learn />
     <Features />
     <Earn />
-    <Partnership />
-    {/* <Download /> */}
     <Contact />
     <InstaFeed />
   </Layout>
