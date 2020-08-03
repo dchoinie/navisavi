4a77c5df17f2db36a1ae9283e737ad3c9fb0748d
Luca Koval <lucakoval@me.com>
2020-08-03 15:18:51 -0400

    Cleaning

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -21,47 +21,39 @@ const Careers = () => {
                 </div>
                 <div className={styles.tableContainer}>
                     <div className={styles.jobRow}>
-                        <div className={styles.jobTitle}>
-                            <a
-                                href="mailto:contact@navi-savi.com?subject=Marketing%20And%20Social%20Media%20Intern%20Application"
-                                className={styles.jobTitleLink}
-                            >
-                                Marketing & Social Media Intern
-                            </a>
-                        </div>
+                        <a
+                            href="mailto:contact@navi-savi.com?subject=Marketing%20And%20Social%20Media%20Intern%20Application"
+                            className={styles.jobTitleLink}
+                        >
+                            Marketing & Social Media Intern
+                        </a>
                         <div className={styles.subInfoContainer}>New York, NY | Stipend | College Credit</div>
                     </div>
                     <div className={styles.jobRow}>
-                        <div className={styles.jobTitle}>
-                            <a
-                                href="mailto:contact@navi-savi.com?subject=Community%20Intern%20Application"
-                                className={styles.jobTitleLink}
-                            >
-                                Community Intern
-                            </a>
-                        </div>
+                        <a
+                            href="mailto:contact@navi-savi.com?subject=Community%20Intern%20Application"
+                            className={styles.jobTitleLink}
+                        >
+                            Community Intern
+                        </a>
                         <div className={styles.subInfoContainer}>New York, NY | Stipend | College Credit</div>
                     </div>
                     <div className={styles.jobRow}>
-                        <div className={styles.jobTitle}>
-                            <a
-                                href="mailto:contact@navi-savi.com?subject=Video%20Content%20Intern%20Application"
-                                className={styles.jobTitleLink}
-                            >
-                                Video Content Intern
-                            </a>
-                        </div>
+                        <a
+                            href="mailto:contact@navi-savi.com?subject=Video%20Content%20Intern%20Application"
+                            className={styles.jobTitleLink}
+                        >
+                            Video Content Intern
+                        </a>
                         <div className={styles.subInfoContainer}>New York, NY | Stipend | College Credit</div>
                     </div>
                     <div className={styles.jobRow}>
-                        <div className={styles.jobTitle}>
-                            <a
-                                href="mailto:contact@navi-savi.com?subject=Community%20And%20Content%20Ambassador%20Application"
-                                className={styles.jobTitleLink}
-                            >
-                                Community & Content Ambassadors
-                            </a>
-                        </div>
+                        <a
+                            href="mailto:contact@navi-savi.com?subject=Community%20And%20Content%20Ambassador%20Application"
+                            className={styles.jobTitleLink}
+                        >
+                            Community & Content Ambassadors
+                        </a>
                         <div className={styles.subInfoContainer}>Remote (Major Cities, World-wide) | Stipend | College Credit</div>
                     </div>
                 </div>
