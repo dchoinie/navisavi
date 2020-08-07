a0c73d083993a782c42b5703e3d02bc9d63053c4
Luca Koval <lucakoval@me.com>
2020-08-07 13:54:28 -0400

    Adding blog post styling

--- src/components/blogPostDetails.js
+++ src/components/blogPostDetails.js
@@ -1,26 +1,24 @@
 import React from "react"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
+import styles from "../styles/BlogPost.module.css"
 
 export default ({ blogPost }) => {
-  return (
-    <div className="flex flex-col justify-center px-6 py-24">
-      <SEO title={blogPost.title} />
-      <div className="max-w-4xl mx-auto flex flex-col">
-        <h2 className="lowercase teal text-xl lg:text-5xl">{blogPost.title}</h2>
-        <h5 className="text-gray-700">By: {blogPost.author}</h5>
-        <p className="text-gray-600">{blogPost.date}</p>
-        <div className="blog">
-          <div
-            dangerouslySetInnerHTML={{
-              __html: blogPost.body.childMarkdownRemark.html,
-            }}
-          ></div>
-        </div>
-        <div className="max-w-md">
-          <Img fluid={blogPost.image.fluid} />
-        </div>
-      </div>
-    </div>
-  )
+  	return (
+	    <div className={styles.container}>
+	        <SEO title={blogPost.title} />
+	        <div className={styles.contentContainer}>
+	        	<div className={styles.infoContainer}>
+	          		<div className={styles.title}>{blogPost.title}</div>
+	          		<div className={styles.details}>by <span className={styles.author}>{blogPost.author}</span> | {blogPost.date}</div>
+	          	</div>
+	          	<div
+	            	dangerouslySetInnerHTML={{
+	              	__html: blogPost.body.childMarkdownRemark.html,
+	            	}}
+	            	className={styles.bodyContainer}
+	          	/>
+	        </div>
+	    </div>
+  	)
 }
